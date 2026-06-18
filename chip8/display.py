546fc5208d8e273879be3aaa9f20cd4a9904742d
Julito-Dev <julianpaez180@gmail.com>
2026-06-18 16:32:40 -0500

    feat: Display methods implemented

--- chip8/display.py
+++ chip8/display.py
@@ -12,3 +12,22 @@ class display:
         self.pixels = [0] * (WIDTH * HEIGHT)
         
     
+    def clear(self):
+        self.pixels = [0] * (WIDTH* HEIGHT)
+    
+    def draw(self, x, y, value):
+        index = y * WIDTH * x
+        collision = self.pixels[index] & value
+        self.pixels[index] ^= value
+        return collision == 1
+    
+    def render(self):
+        self.screen.fill((0,0,0))
+        for i, pixel in enumerate(self.pixels):
+            if pixel:
+                x = (i % WIDTH) * SCALE
+                y = (i % HEIGHT) * SCALE
+                pygame.draw.rect(self.screen, (255,255,255), x, y, SCALE, SCALE)
+                
+        pygame.display.flip()
+                
