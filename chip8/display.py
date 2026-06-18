9549d1a5b213a3fccc412a04ec6007e4916828b6
Julito-Dev <julianpaez180@gmail.com>
2026-06-18 17:00:43 -0500

    feat: add extra methods in display class

--- chip8/display.py
+++ chip8/display.py
@@ -10,10 +10,27 @@ class display:
         self.screen = pygame.display.set_mode((WIDTH * SCALE, HEIGHT * SCALE))
         pygame.display.set_caption("Chip-8")
         self.pixels = [0] * (WIDTH * HEIGHT)
-        
+        self.clock = pygame.time.Clock()
     
     def clear(self):
         self.pixels = [0] * (WIDTH* HEIGHT)
+        
+    def get_pixel(self, x, y):
+        if 0 <= x < WIDTH and 0 <= y < HEIGHT:
+            return self.pixels[y * WIDTH + x]
+        return 0
+
+    def set_pixel(self, x,y, value):
+        if 0<= x < WIDTH and 0 <= y < HEIGHT:
+            self.pixels[y * WIDTH + x ] = value
+            
+    def toggle_pixel(self, x, y):
+        if 0<= x < WIDTH and 0 <= y < HEIGHT:
+            index = y * WIDTH + x
+            collision = self.pixels[index] == 1
+            self.pixels[index] ^= 1 # Toggle
+            return collision
+        return False
     
     def draw(self, x, y, value):
         index = y * WIDTH + x
@@ -26,8 +43,19 @@ class display:
         for i, pixel in enumerate(self.pixels):
             if pixel:
                 x = (i % WIDTH) * SCALE
-                y = (i // HEIGHT) * SCALE
-                pygame.draw.rect(self.screen, (255,255,255), x, y, SCALE, SCALE)
+                y = (i // WIDTH) * SCALE
+                pygame.draw.rect(self.screen, (255,255,255), (x, y, SCALE, SCALE))
                 
         pygame.display.flip()
+        self.clock.tick(60)
+    
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return False
+        
+        return True
+
+    
                 
